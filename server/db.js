71ced2b0df9dedecb0b6f78707e38dd4dfdd8105
raghavdolyar <raghavdolyardell@gmail.com>
2026-07-17 13:37:31 +0530

    implemented basic server

---
+++ server/db.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import pkg from 'pg';
+
+dotenv.config();
+
+const { Pool, types } = pkg;
+
+types.setTypeParser(1082, val => val);
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+pool.on('connect', () => {
+  console.log('connected to neon postgres');
+});
+
+pool.on('error', err => {
+  console.error('unexpected postgres error :', err);
+  process.exit(-1);
+});
+
+export default pool;
---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import router from './routes/auth';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'spendora is running' });
+});
+
+app.use('/api/auth', router);
+
+app.listen(PORT, () => {
+  console.log(`server running on address http://localhost:${PORT}`);
+});
