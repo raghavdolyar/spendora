dc5e4ad479683e0f7ac9dcc504c51894b502d22e
raghavdolyar <raghavdolyardell@gmail.com>
2026-07-17 17:58:42 +0530

    prepared for deployment

--- server/server.js
+++ server/server.js
@@ -14,7 +14,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
